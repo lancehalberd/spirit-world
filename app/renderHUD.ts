4aa7f6d428552197a205f57610ef707c03875a68
Chris Brewer <chris.brewer@adroll.com>
2022-01-08 22:19:28 -0600

    Fix area cues weren't rendering.

--- app/renderHUD.ts
+++ app/renderHUD.ts
@@ -123,9 +123,9 @@ export function renderHUD(context: CanvasRenderingContext2D, state: GameState): 
             size: 16,
         });
     }
-    for (const object of (state.areaInstance?.objects || [])) {
-        if (object.drawPriority === 'hud') {
-            object.render(context, state);
+    for (const effects of (state.areaInstance?.effects || [])) {
+        if (effects.drawPriority === 'hud') {
+            effects.render(context, state);
         }
     }
 }
