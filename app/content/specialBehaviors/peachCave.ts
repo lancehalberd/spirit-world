da1bbe9ae5f63f13907e3dfdac658def4f67dd22
Chris Brewer <chris.brewer@adroll.com>
2022-01-13 10:56:54 -0600

    Add missing file for Hillary.

---
+++ app/content/specialBehaviors/peachCave.ts
@@ -0,0 +1,13 @@
+import { specialBehaviorsHash } from 'app/content/specialBehaviors/specialBehaviorsHash';
+
+import { AreaInstance, GameState } from 'app/types';
+
+specialBehaviorsHash.peachCave = {
+    type: 'area',
+    apply(state: GameState, area: AreaInstance) {
+        const bossDefeated = !!state.savedState.objectFlags['peachCave:boss'];
+        if (bossDefeated) {
+            area.dark = Math.max(area.definition.dark, 90);
+        }
+    },
+};
