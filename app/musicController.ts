92b538dac5c42c9af524fbd2739f82711790a323
Chris Brewer <chris.brewer@adroll.com>
2022-02-05 13:01:08 -1000

    Fix whitespace.

--- app/musicController.ts
+++ app/musicController.ts
@@ -7,67 +7,67 @@ export { stopSound } from 'app/utils/sounds';
 
 
 export const updateMusic = (): void => {
-	const state = getState();
+    const state = getState();
     if (!state?.gameHasBeenInitialized) {
         return;
     }
     const muted = state.settings.muteAllSounds;
-	if (state.scene !== 'game') {
-		playTrack('mainTheme', 0, muted);
-		return;
-	}
-	const bosses = [...state.areaInstance.enemies, ...state.alternateAreaInstance.enemies].filter(
+    if (state.scene !== 'game') {
+        playTrack('mainTheme', 0, muted);
+        return;
+    }
+    const bosses = [...state.areaInstance.enemies, ...state.alternateAreaInstance.enemies].filter(
         e => e.status !== 'gone' && e.healthBarTime > 0 && e.definition.type === 'boss' && e.isFromCurrentSection(state)
     ) as Enemy[];
     if (bosses.length) {
-    	// The boss track is several different pieces, so we want to make sure not to restart the intro
-    	// when any of them are still playing. Note that the tracks will automatically transition
-    	// because of built in logic so we don't need to add logic to play the other tracks.
-    	// Eventually it might be fun to add logic here to manipulate which sections play based on
-    	// how the fight is going, for example more intense music when the boss is enraged.
-    	if (!isTrackPlaying('bossIntro') && !isTrackPlaying('bossA') && !isTrackPlaying('bossB')) {
+        // The boss track is several different pieces, so we want to make sure not to restart the intro
+        // when any of them are still playing. Note that the tracks will automatically transition
+        // because of built in logic so we don't need to add logic to play the other tracks.
+        // Eventually it might be fun to add logic here to manipulate which sections play based on
+        // how the fight is going, for example more intense music when the boss is enraged.
+        if (!isTrackPlaying('bossIntro') && !isTrackPlaying('bossA') && !isTrackPlaying('bossB')) {
             playTrack('bossIntro', 0, muted);
-    	}
+        }
     } else if (state.location.zoneKey === 'overworld'
-		|| state.location.zoneKey === 'sky'
-		|| state.location.zoneKey === 'underwater'
-	) {
-		playTrack('mainTheme', 0, muted);
-	} else if (state.location.zoneKey === 'peachCave'
-		|| state.location.zoneKey === 'peachCaveWater'
-		|| state.location.zoneKey === 'riverTemple'
-		|| state.location.zoneKey === 'riverTempleWater'
-		|| state.location.zoneKey === 'lakeTunnel'
-	) {
-		playTrack('caveTheme', 0, muted);
-	} else if (state.location.zoneKey === 'tomb' || state.location.zoneKey === 'waterfallTower') {
-		playTrack('tombTheme', 0, muted);
-	} else if (state.location.zoneKey === 'waterfallCave'
-		|| state.location.zoneKey === 'holyCityInterior'
-		|| state.location.zoneKey === 'treeVillage'
-	) {
-		playTrack('idleTheme', 0, muted);
-	} else if (state.location.zoneKey === 'warTemple' || state.location.zoneKey === 'forestTemple') {
-		playTrack('dungeonTheme', 0, muted);
-	} else if (state.location.zoneKey === 'cocoon'
-		|| state.location.zoneKey === 'helix'
-		|| state.location.zoneKey === 'crater'
-	) {
-		playTrack('cocoonTheme', 0, muted);
-	} else if (state.location.zoneKey === 'staffTower') {
-		// Play a different track when the tower is activated later.
-		playTrack('caveTheme', 0, muted);
-	} else if (state.location.zoneKey === 'caves') {
-		if (state.location.areaGridCoords[0] === 1 && state.location.areaGridCoords[1] === 0) {
-			// This is the fertility temple.
-			playTrack('idleTheme', 0, muted);
-		} else {
-			playTrack('caveTheme', 0, muted);
-		}
-	}
+        || state.location.zoneKey === 'sky'
+        || state.location.zoneKey === 'underwater'
+    ) {
+        playTrack('mainTheme', 0, muted);
+    } else if (state.location.zoneKey === 'peachCave'
+        || state.location.zoneKey === 'peachCaveWater'
+        || state.location.zoneKey === 'riverTemple'
+        || state.location.zoneKey === 'riverTempleWater'
+        || state.location.zoneKey === 'lakeTunnel'
+    ) {
+        playTrack('caveTheme', 0, muted);
+    } else if (state.location.zoneKey === 'tomb' || state.location.zoneKey === 'waterfallTower') {
+        playTrack('tombTheme', 0, muted);
+    } else if (state.location.zoneKey === 'waterfallCave'
+        || state.location.zoneKey === 'holyCityInterior'
+        || state.location.zoneKey === 'treeVillage'
+    ) {
+        playTrack('idleTheme', 0, muted);
+    } else if (state.location.zoneKey === 'warTemple' || state.location.zoneKey === 'forestTemple') {
+        playTrack('dungeonTheme', 0, muted);
+    } else if (state.location.zoneKey === 'cocoon'
+        || state.location.zoneKey === 'helix'
+        || state.location.zoneKey === 'crater'
+    ) {
+        playTrack('cocoonTheme', 0, muted);
+    } else if (state.location.zoneKey === 'staffTower') {
+        // Play a different track when the tower is activated later.
+        playTrack('caveTheme', 0, muted);
+    } else if (state.location.zoneKey === 'caves') {
+        if (state.location.areaGridCoords[0] === 1 && state.location.areaGridCoords[1] === 0) {
+            // This is the fertility temple.
+            playTrack('idleTheme', 0, muted);
+        } else {
+            playTrack('caveTheme', 0, muted);
+        }
+    }
 }
 
 export function playSound(key: string) {
-	const state = getState();
-	return playSoundProper(key, state.settings.muteAllSounds);
+    const state = getState();
+    return playSoundProper(key, state.settings.muteAllSounds);
 }
--- app/types/scriptEvents.ts
+++ app/types/scriptEvents.ts
@@ -1,71 +1,71 @@
 import { Frame, LootData } from 'app/types';
 
 export interface ShowTextBoxScriptEvent {
-	type: 'showTextBox'
-	textPage: Frame[][]
+    type: 'showTextBox'
+    textPage: Frame[][]
 }
 export interface ClearTextBoxScriptEvent {
-	type: 'clearTextBox'
+    type: 'clearTextBox'
 }
 export interface WaitScriptEvent {
-	type: 'wait'
-	blockFieldUpdates?: boolean
-	duration?: number
-	keys?: number[]
+    type: 'wait'
+    blockFieldUpdates?: boolean
+    duration?: number
+    keys?: number[]
 }
 export type WaitActiveScriptEvent = WaitScriptEvent & {
-	time: number
+    time: number
 }
 export interface ShowChoiceBoxScriptEvent {
-	type: 'showChoiceBox'
-	prompt?: string
-	choices: {
+    type: 'showChoiceBox'
+    prompt?: string
+    choices: {
         text: string
         key: string
     }[]
 }
 export type ShowChoiceBoxActiveScriptEvent = ShowChoiceBoxScriptEvent & {
-	choiceIndex: number
+    choiceIndex: number
 }
 export interface SetFlagScriptEvent {
-	type: 'setFlag'
-	flag: string
-	value?: boolean | number | string
+    type: 'setFlag'
+    flag: string
+    value?: boolean | number | string
 }
 export interface ClearFlagScriptEvent {
-	type: 'clearFlag'
-	flag: string
+    type: 'clearFlag'
+    flag: string
 }
 export interface RefreshAreaLogicScriptEvent {
-	type: 'refreshAreaLogic'
-	// Perhaps define whether to use a transition here.
+    type: 'refreshAreaLogic'
+    // Perhaps define whether to use a transition here.
 }
 export interface GainLootScriptEvent {
-	type: 'gainLoot'
-	lootDefinition: LootData
+    type: 'gainLoot'
+    lootDefinition: LootData
 }
 export interface PlaySoundScriptEvent {
-	type: 'playSound'
-	sound: string
+    type: 'playSound'
+    sound: string
 }
 export interface RunDialogueScriptScriptEvent {
-	type: 'runDialogueScript'
-	// Dialogue is currently organized by npc
-	npcKey: string
-	// NPC Dialogue can map a key to a particular dialogue on the `mappedOptions` property.
-	scriptKey: string
+    type: 'runDialogueScript'
+    // Dialogue is currently organized by npc
+    npcKey: string
+    // NPC Dialogue can map a key to a particular dialogue on the `mappedOptions` property.
+    scriptKey: string
 }
 
 export interface AttemptPurchaseScriptEvent {
-	type: 'attemptPurchase'
-	cost: number
-	// Instead of using string here, these could also be parsed arrays of ScriptEvents
-	successScript: string
-	failScript: string
+    type: 'attemptPurchase'
+    cost: number
+    // Instead of using string here, these could also be parsed arrays of ScriptEvents
+    successScript: string
+    failScript: string
 }
 
 export interface RestScriptEvent {
-	type: 'rest'
+    type: 'rest'
 }
 export interface ScreenShakeScriptEvent {
     type: 'screenShake'
@@ -85,24 +85,24 @@ export interface StopScreenShakeScriptEvent {
 }
 
 export type ScriptEvent
-	= AttemptPurchaseScriptEvent
-	| ClearFlagScriptEvent
-	| ClearTextBoxScriptEvent
-	| GainLootScriptEvent
-	| PlaySoundScriptEvent
-	| RefreshAreaLogicScriptEvent
-	| RestScriptEvent
-	| RunDialogueScriptScriptEvent
+    = AttemptPurchaseScriptEvent
+    | ClearFlagScriptEvent
+    | ClearTextBoxScriptEvent
+    | GainLootScriptEvent
+    | PlaySoundScriptEvent
+    | RefreshAreaLogicScriptEvent
+    | RestScriptEvent
+    | RunDialogueScriptScriptEvent
     | ScreenShakeScriptEvent
-	| SetFlagScriptEvent
-	| ShowChoiceBoxScriptEvent
-	| ShowTextBoxScriptEvent
+    | SetFlagScriptEvent
+    | ShowChoiceBoxScriptEvent
+    | ShowTextBoxScriptEvent
     | StartScreenShakeScriptEvent
     | StopScreenShakeScriptEvent
-	| WaitScriptEvent
-	;
+    | WaitScriptEvent
+    ;
 
 export type ActiveScriptEvent
-	= ShowChoiceBoxActiveScriptEvent
-	| WaitActiveScriptEvent
-	;
+    = ShowChoiceBoxActiveScriptEvent
+    | WaitActiveScriptEvent
+    ;
