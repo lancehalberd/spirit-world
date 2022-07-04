83cc3045cf12bef8af98d7a741269e03dfc6d0cf
Chris Brewer <chris@chrisflash.com>
2022-07-03 20:50:28 -0500

    Clean up tomb entrance graphics, add waterfall graphics test.

--- app/content/objects/decoration.ts
+++ app/content/objects/decoration.ts
@@ -1,7 +1,10 @@
 import { FRAME_LENGTH } from 'app/gameConstants';
+import { createCanvasAndContext } from 'app/dom';
+import { drawFrameAt } from 'app/utils/animations';
+import { requireImage } from 'app/utils/images';
 import {
-    AreaInstance, DrawPriority, GameState,
-    ObjectInstance, ObjectStatus, DecorationDefinition,
+    AreaInstance, DrawPriority, Frame, FrameWithPattern, GameState,
+    ObjectInstance, ObjectStatus, DecorationDefinition, Rect,
 } from 'app/types';
 
 export class Decoration implements ObjectInstance {
@@ -35,9 +38,42 @@ export class Decoration implements ObjectInstance {
     }
 }
 
+const waterfallImage = requireImage('gfx/tiles/waterfalltilesdeep.png');
+
+const waterfallTopPattern: FrameWithPattern = {image: waterfallImage, x: 16, y: 12, w: 32, h: 20};
+const waterfallMiddlePattern: FrameWithPattern = {image: waterfallImage, x: 16, y: 32, w: 32, h: 16};
+const waterfallBottomPattern: FrameWithPattern = {image: waterfallImage, x: 16, y: 48, w: 32, h: 16};
+const waterfallLeftPattern: FrameWithPattern = {image: waterfallImage, x: 0, y: 32, w: 16, h: 16};
+const waterfallRightPattern: FrameWithPattern = {image: waterfallImage, x: 48, y: 32, w: 16, h: 16};
+
+const waterfallTL: Frame = {image: waterfallImage, x: 0, y: 12, w: 16, h: 20};
+const waterfallTR: Frame = {image: waterfallImage, x: 48, y: 12, w: 16, h: 20};
+const waterfallBL: Frame = {image: waterfallImage, x: 0, y: 48, w: 16, h: 16};
+const waterfallBR: Frame = {image: waterfallImage, x: 48, y: 48, w: 16, h: 16};
+
+
+function fillPattern(context: CanvasRenderingContext2D, frame: FrameWithPattern, target: Rect, offset = {x: 0, y: 0}) {
+    if (!frame.pattern ) {
+        const [patternCanvas, patternContext] = createCanvasAndContext(frame.w, frame.h);
+        drawFrameAt(patternContext, frame, {x: 0, y: 0});
+        frame.pattern = context.createPattern(patternCanvas, 'repeat');
+    }
+    context.save();
+        context.translate(target.x + offset.x, target.y + offset.y);
+        context.fillStyle = frame.pattern;
+        context.fillRect(-offset.x, -offset.y, target.w, target.h);
+    context.restore();
+}
+
+const useWaterfallTiles = false;
+
 export const decorationTypes = {
     waterfall: {
         render(context: CanvasRenderingContext2D, state: GameState, decoration: Decoration) {
+            if (useWaterfallTiles) {
+                renderWaterfallTiles(context, state, decoration);
+                return;
+            }
             // Draw this 8 pixels past the bottom so that y sorting looks better.
             const h = decoration.h + 8;
             context.save();
@@ -88,6 +124,61 @@ export const decorationTypes = {
             context.restore();
         }
     }
+}
+
+function renderWaterfallTiles(context: CanvasRenderingContext2D, state: GameState, decoration: Decoration) {
+    let y = decoration.y - decoration.h - 4;
+    drawFrameAt(context, waterfallTL, {
+        ...waterfallTL, x: decoration.x - 8, y,
+    });
+    fillPattern(context, waterfallTopPattern, {
+        x: decoration.x + 8,
+        y,
+        w: decoration.w - 16,
+        h: waterfallTopPattern.h,
+    });
+    drawFrameAt(context, waterfallTR, {
+        ...waterfallTR, x: decoration.x + decoration.w - 8, y,
+    });
+
+    y += waterfallTR.h;
+    const offset = {x: 0, y: Math.round(decoration.animationTime / 20) % 16};
+    // Left Side
+    fillPattern(context, waterfallLeftPattern, {
+        x: decoration.x - 8,
+        y,
+        w: waterfallLeftPattern.w,
+        h: decoration.h - 24,
+    }, offset);
+    // Middle
+    fillPattern(context, waterfallMiddlePattern, {
+        x: decoration.x + 8,
+        y,
+        w: decoration.w - 16,
+        h: decoration.h - 24,
+    }, offset);
+    // Right Side
+    fillPattern(context, waterfallRightPattern, {
+        x: decoration.x + decoration.w - 8,
+        y,
+        w: waterfallRightPattern.w,
+        h: decoration.h - 24,
+    }, offset);
+
+    y = decoration.y - 8;
+    // Bottom
+    drawFrameAt(context, waterfallBL, {
+        ...waterfallBL, x: decoration.x - 8, y,
+    });
+    fillPattern(context, waterfallBottomPattern, {
+        x: decoration.x + 8,
+        y,
+        w: decoration.w - 16,
+        h: waterfallBottomPattern.h,
+    });
+    drawFrameAt(context, waterfallBR, {
+        ...waterfallBR, x: decoration.x + decoration.w - 8, y,
+    });
 }
 
 export type DecorationType = keyof typeof decorationTypes;
