7d6b12bf5cc58e515b5b0eaa9ba2013871bc91ee
Chris Brewer <chris@chrisflash.com>
2023-02-27 19:04:14 -0600

    Improve spark graphics.

--- app/content/effects/spark.ts
+++ app/content/effects/spark.ts
@@ -1,22 +1,25 @@
 import { FRAME_LENGTH } from 'app/gameConstants';
+import { renderLightningCircle, renderLightningRay } from 'app/render/renderLightning'
 import { addEffectToArea, removeEffectFromArea } from 'app/utils/effects';
 import { hitTargets } from 'app/utils/field';
 
 import {
-    AreaInstance, DrawPriority, EffectInstance,
-    Frame, GameState, Point,
+    AreaInstance, Circle, DrawPriority, EffectInstance,
+    Frame, GameState, HitProperties, Point, Ray,
 } from 'app/types';
 
 interface Props {
-    x: number,
-    y: number,
-    z?: number,
-    damage?: number,
-    vx?: number,
-    vy?: number,
-    vz?: number,
-    az?: number,
-    ttl?: number,
+    x: number
+    y: number
+    z?: number
+    damage?: number
+    vx?: number
+    vy?: number
+    vz?: number
+    az?: number
+    ttl?: number
+    hitCircle?: Circle
+    hitRay?: Ray
 }
 
 export class Spark implements EffectInstance, Props {
@@ -25,28 +28,44 @@ export class Spark implements EffectInstance, Props {
     isEffect = <const>true;
     isEnemyAttack = true;
     frame: Frame;
-    damage: number;
-    x: number;
-    y: number;
-    z: number = 0;
-    vz: number = 0;
-    vx: number;
-    vy: number;
-    az: number;
-    radius: number = 3;
+    damage: number = this.props.damage ?? 1;
+    x: number = this.props.x;
+    y: number = this.props.y;
+    z: number = this.props.z ?? 0;
+    vx: number = this.props.vx ?? 0;
+    vy: number = this.props.vy ?? 0;
+    vz: number = this.props.vz ?? 0;
+    az: number = this.props.az ?? -0.3;
+    hitCircle = this.props.hitCircle
+        || ((!this.vx && !this.vy) ? {x: 0, y: 0, r: 6} : undefined);
+    hitRay = this.props.hitRay
+        || ((this.vx || this.vy) ? {x1: -4 * this.vx, y1 : -4 * this.vy, x2: 4 * this.vx, y2: 4 * this.vy, r: 2} : undefined);
     animationTime = 0;
-    speed = 0;
-    ttl: number;
-    constructor({x, y, z = 0, vx = 0, vy = 0, vz = 0, az = -0.3, damage = 1, ttl = 2000}: Props) {
-        this.damage = damage;
-        this.x = x;
-        this.y = y;
-        this.z = z;
-        this.vx = vx;
-        this.vy = vy;
-        this.vz = vz;
-        this.az = az;
-        this.ttl = ttl;
+    ttl: number = this.props.ttl ?? 2000;
+    constructor(readonly props: Props) { }
+    getHitProperties(): HitProperties {
+        const hitProperties: HitProperties = {
+            damage: this.damage,
+            element: 'lightning',
+            hitAllies: true,
+            knockAwayFrom: {x: this.x, y: this.y},
+        }
+        if (this.hitRay) {
+            hitProperties.hitRay = {
+                ...this.hitRay,
+                x1: this.x + this.hitRay.x1,
+                y1: this.y + this.hitRay.y1,
+                x2: this.x + this.hitRay.x2,
+                y2: this.y + this.hitRay.y2,
+            };
+        } else {
+            hitProperties.hitCircle = {
+                ...this.hitCircle,
+                x: this.x + this.hitCircle.x,
+                y: this.y + this.hitCircle.y
+            };
+        }
+        return hitProperties;
     }
     update(state: GameState) {
         this.x += this.vx;
@@ -58,27 +77,28 @@ export class Spark implements EffectInstance, Props {
         if (this.animationTime >= this.ttl) {
             removeEffectFromArea(state, this);
         } else {
-            hitTargets(state, this.area, {
-                damage: this.damage,
-                hitCircle: {x: this.x, y: this.y, r: this.radius},
-                element: 'lightning',
-                hitAllies: true,
-                knockAwayFrom: {x: this.x, y: this.y},
-            });
+            hitTargets(state, this.area, this.getHitProperties());
         }
     }
     render(context: CanvasRenderingContext2D, state: GameState) {
-        context.fillStyle = 'yellow';
-        context.save();
-            context.globalAlpha *= 0.6;
-            context.fillRect(this.x - this.radius, this.y - this.radius - this.z, 2 * this.radius, 2 * this.radius);
-        context.restore();
-        context.beginPath();
-        context.strokeStyle = 'yellow';
-        context.lineWidth = 2;
-        context.moveTo(this.x - 2 * this.vx, this.y - 2 * this.vy);
-        context.lineTo(this.x + 2 * this.vx, this.y + 2 * this.vy);
-        context.stroke();
+        if (this.hitRay) {
+            const theta = Math.atan2(this.hitRay.y2 - this.hitRay.y1, this.hitRay.x2 - this.hitRay.x1);
+            const px = 2 * Math.cos(theta);
+            const py = 2 * Math.sin(theta);
+            renderLightningRay(context, {
+                x1: this.x + this.hitRay.x1 - px,
+                y1: this.y + this.hitRay.y1 - py,
+                x2: this.x + this.hitRay.x2 + px,
+                y2: this.y + this.hitRay.y2 + py,
+                r: this.hitRay.r + 2
+            }, 1, 20);
+        } else {
+            renderLightningCircle(context, {
+                x: this.x + this.hitCircle.x,
+                y: this.y + this.hitCircle.y,
+                r: this.hitCircle.r + 2
+            }, 1, 20);
+        }
     }
 }
 
