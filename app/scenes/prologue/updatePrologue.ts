7e2252c30ce29d16a9ccca7c069b1a54838072f7
Chris Brewer <chris@chrisflash.com>
2024-07-26 11:25:41 -0500

    Fix regular controls for the prologue, increment version.

--- app/scenes/prologue/updatePrologue.ts
+++ app/scenes/prologue/updatePrologue.ts
@@ -1,33 +1,34 @@
 import { FRAME_LENGTH, GAME_KEY } from 'app/gameConstants';
-import { isGameKeyDown, wasGameKeyPressed, wasMenuConfirmKeyPressed } from 'app/userInput';
+import { isGameKeyDown, isKeyboardKeyDown, KEY, wasGameKeyPressed, wasMenuConfirmKeyPressed } from 'app/userInput';
 import { isATrackPlaying } from 'app/utils/sounds';
 
 export function updatePrologue(state: GameState) {
-    if (!state.paused) {
+    if (isKeyboardKeyDown(KEY.SHIFT)) {
         if (isGameKeyDown(state, GAME_KEY.LEFT)) {
-            state.prologueTime -= FRAME_LENGTH;
+            state.prologueTime -= 5 * FRAME_LENGTH;
         } else if (isGameKeyDown(state, GAME_KEY.RIGHT)) {
-            state.prologueTime += 2 * FRAME_LENGTH;
+            state.prologueTime += 5 * FRAME_LENGTH;
         } else if (isGameKeyDown(state, GAME_KEY.UP)) {
-            state.prologueTime += 10 * FRAME_LENGTH;
+            state.prologueTime += 25 * FRAME_LENGTH;
         } else  if (isGameKeyDown(state, GAME_KEY.DOWN)) {
-            state.prologueTime -= 10 * FRAME_LENGTH;
-        } else {
-            if (state.prologueTime < 1000 && !isATrackPlaying() && !state.settings.muteAllSounds) {
-                // Delay longer if the music hasn't started playing (unless the game is muted).
-                state.prologueTime += FRAME_LENGTH / 4;
-            } else {
-                state.prologueTime += FRAME_LENGTH;
-            }
+            state.prologueTime -= 25 * FRAME_LENGTH;
         }
-        state.prologueTime = Math.max(0, state.prologueTime);
+    } else {
+        if (state.prologueTime < 1000 && !isATrackPlaying() && !state.settings.muteAllSounds) {
+            // Delay longer if the music hasn't started playing (unless the game is muted).
+            state.prologueTime += FRAME_LENGTH / 4;
+        } else {
+            state.prologueTime += FRAME_LENGTH;
+        }
     }
-    if (wasGameKeyPressed(state, GAME_KEY.MENU)) {
-        state.paused = !state.paused;
-    }
+    state.prologueTime = Math.max(0, state.prologueTime);
     // This timestamp needs to be kept in sync with the rendering code in `renderPrologue` so that we switch
     // to the title shortly after the prologue fade to black.
-    if (state.prologueTime >= 74000 || wasMenuConfirmKeyPressed(state) || wasGameKeyPressed(state, GAME_KEY.MAP)) {
+    if (state.prologueTime >= 74000
+        || wasMenuConfirmKeyPressed(state)
+        || wasGameKeyPressed(state, GAME_KEY.MENU)
+        || wasGameKeyPressed(state, GAME_KEY.MAP)
+    ) {
         state.scene = 'title';
         return;
     }
