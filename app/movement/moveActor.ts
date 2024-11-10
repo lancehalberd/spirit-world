52cef095d5382e9f35afc5171d037a792e890af2
Chris Brewer <chris@chrisflash.com>
2024-11-10 13:56:23 -0600

    Update version and fix softlock.

--- app/movement/moveActor.ts
+++ app/movement/moveActor.ts
@@ -245,3 +245,23 @@ export function intersectRectangles({x, y, w, h}: Rect, {x: X, y: Y, w: W, h: H}
     const l = Math.max(x, X), t = Math.max(y, Y), r = Math.min(x + w, X + W), b = Math.min(y + h, Y + H);
     return {x: l, y: t, w: r - l, h: b - t};
 }
+
+// Move an actor's so that the middle of their hitboxes moves towards a target location at a given speed per frame
+// and then returns the distance remaining.
+export function moveActorTowardsLocation(
+    state: GameState,
+    actor: Actor,
+    {x, y}: Point,
+    speed = 1
+): number {
+    const hitbox = actor.getMovementHitbox();
+    const dx = x - (hitbox.x + hitbox.w / 2), dy = y - (hitbox.y + hitbox.h / 2);
+    actor.d = getDirection(dx, dy);
+    const mag = Math.sqrt(dx * dx + dy * dy);
+    if (mag > speed) {
+        moveActor(state, actor, speed * dx / mag, speed * dy / mag, {boundingBox: false});
+        return mag - speed;
+    }
+    moveActor(state, actor, dx, dy, {boundingBox: false});
+    return 0;
+}
