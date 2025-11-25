cb6539a244af57ee3b4cbf8acbfa36b8b2da3276
unknown <Evan20000@gmail.com>
2025-11-24 21:46:43 -0700

    Gave Sentrybot a cute pulselaser effect.

--- app/content/effects/laserBeam.ts
+++ app/content/effects/laserBeam.ts
@@ -5,6 +5,7 @@ import { removeEffectFromArea } from 'app/utils/effects';
 import {isEnemyDefeated} from 'app/utils/enemies';
 import { hitTargets } from 'app/utils/field';
 import { getTileBehaviors} from 'app/utils/getBehaviors';
+import Random from 'app/utils/Random';
 
 
 function truncateRay(state: GameState, area: AreaInstance, ray: Ray): Ray {
@@ -44,9 +45,11 @@ interface Props {
     tx: number
     ty: number
     radius?: number
+	baseradius?: number
     duration?: number
     tellDuration?: number
     ignoreWalls?: boolean
+	monocolor?: boolean
     damage?: number
     delay?: number
     source: Enemy
@@ -58,9 +61,12 @@ const FADE_DURATION = 100;
 // This many pixels is added to the appearance + warning box for the laser.
 // This makes it more intuitive to dodge.
 const visualPadding = 4;
+const sentryPadding = 1;
 function padRay(ray: Ray, padding: number) {
     return {...ray, r: ray.r + padding};
 }
+
+
 
 export class LaserBeam implements EffectInstance, Props {
     area: AreaInstance = null;
@@ -73,6 +79,7 @@ export class LaserBeam implements EffectInstance, Props {
     tx = this.props.tx | 0;
     ty = this.props.ty | 0;
     radius = this.props.radius ?? 6;
+	baseradius = this.props.baseradius ?? 6;
     delay = this.props.delay;
     duration = this.props.duration ?? 200;
     ignoreWalls = this.props.ignoreWalls;
@@ -197,7 +204,6 @@ export function drawLaser(context: CanvasRenderingContext2D, ray: Ray, alpha: nu
             0,
             ray.r,
         );
-
         // Add color stops
         // pink #f9c8c8 rgba(249,200,200,255)
         // red #ef3b3b rgba(239,59,59,255)
@@ -212,10 +218,174 @@ export function drawLaser(context: CanvasRenderingContext2D, ray: Ray, alpha: nu
         gradient.addColorStop(0.8, "rgba(207,35,64,0.8)");
         gradient.addColorStop(0.98, "rgba(207,35,64,0.2)");
         gradient.addColorStop(1, "rgba(207,35,64,0)");
+		
+		gradient.addColorStop(0.98, "rgba(207,35,64,0.2)");
+        gradient.addColorStop(1, "rgba(207,35,64,0)");
 
         // Set the fill style and draw a rectangle
         context.fillStyle = gradient;
+		context.beginPath();
         const mag = Math.sqrt(dx * dx + dy *dy);
-        context.fillRect(0, -ray.r, mag, ray.r * 2);
+        //context.fillRect(0, -ray.r, mag, ray.r * 2);
+        // Draw a circle at the start of the beam.
+        context.arc(ray.r, 0, ray.r, 0, 2 * Math.PI);
+        // Draw a rectangle starting at the mid point of the circle.
+        context.rect(ray.r, -ray.r, mag - ray.r, ray.r * 2);
+        context.fill();
+		
+    context.restore();
+}
+
+export class SentryBotLaser implements EffectInstance, Props {
+    area: AreaInstance = null;
+    isEffect = <const>true;
+    isEnemyAttack = true;
+    frame: Frame;
+    damage = this.props.damage ?? 2;
+    sx = this.props.sx | 0;
+    sy = this.props.sy | 0;
+    tx = this.props.tx | 0;
+    ty = this.props.ty | 0;
+    radius = this.props.radius ?? 3;
+	baseradius = this.props.baseradius ?? 3;
+    delay = this.props.delay;
+    duration = this.props.duration ?? 200;
+    ignoreWalls = this.props.ignoreWalls;
+    totalTellDuration = this.props.tellDuration ?? 0;
+    tellDuration = this.props.tellDuration ?? 0;
+    source = this.props.source;
+    beforeUpdate = this.props.beforeUpdate;
+    z: number = 0;
+    animationTime = 0;
+    done = false;
+    memoizedHitRay: Ray;
+    constructor(public props: Props) {}
+    getHitRay(state: GameState): Ray {
+        if (this.memoizedHitRay) {
+            return this.memoizedHitRay;
+        }
+        const baseRay = {
+            x1: this.sx, y1: this.sy,
+            x2: this.tx, y2: this.ty,
+            r: this.radius,
+        };
+        if (this.ignoreWalls) {
+            return baseRay
+        }
+        this.memoizedHitRay = truncateRay(state, this.area, baseRay);
+        return this.memoizedHitRay;
+    }
+    update(state: GameState) {
+        if (this.beforeUpdate) {
+            this.beforeUpdate(state, this);
+        }
+        if (this.source && isEnemyDefeated(this.source)) {
+            this.done = true;
+            removeEffectFromArea(state, this);
+            return;
+        }
+        // We only memoize the hit ray for a single frame before recalculating it.
+        delete this.memoizedHitRay;
+        if (this.delay > 0) {
+            this.delay -= FRAME_LENGTH;
+            return;
+        }
+        if (this.tellDuration > 0) {
+            this.tellDuration -= FRAME_LENGTH;
+            return;
+        }
+        this.animationTime += FRAME_LENGTH;
+        if (this.animationTime <= this.duration) {
+            // TODO: prevent hitting targets that are below a ledge from the source of the beam unless ignoring walls.
+            hitTargets(state, this.area, {
+                damage: this.damage,
+                hitRay: this.getHitRay(state),
+                hitAllies: true,
+                knockAwayFromHit: true,
+                source: this.source,
+            });
+        }
+        if (this.animationTime >= this.duration + FADE_DURATION) {
+            this.done = true;
+            removeEffectFromArea(state, this);
+        }
+    }
+    render(context: CanvasRenderingContext2D, state: GameState) {
+        // never draw the laser if the duration is 0.
+        if (this.delay > 0 || this.duration === 0) {
+            return;
+        }
+        if (this.tellDuration > 0) {
+            return;
+        }
+        const p = (this.animationTime - this.duration) / FADE_DURATION;
+        const alpha = 1 - Math.max(0, Math.min(1, p));
+        drawSingleColorLaser(context, padRay(this.getHitRay(state), sentryPadding), alpha);
+    }
+    renderShadow(context: CanvasRenderingContext2D, state: GameState) {
+       if (this.delay > 0) {
+           return;
+       }
+       if (this.tellDuration > 0) {
+           const ray = padRay(this.getHitRay(state), sentryPadding);
+           renderDamageWarning(context, {
+                ray,
+                duration: this.totalTellDuration,
+                time: this.totalTellDuration - this.tellDuration,
+            });
+
+           /* const ray = this.getHitRay(state);
+            context.save();
+                context.translate(ray.x1, ray.y1);
+                const dx = ray.x2 - ray.x1;
+                const dy = ray.y2 - ray.y1;
+                const mag = Math.sqrt(dx * dx + dy *dy);
+                const theta = Math.atan2(dy, dx);
+                context.rotate(theta);
+                context.fillStyle = 'red';
+                context.globalAlpha *= 0.5;
+                context.fillRect(0, -ray.r, mag, 1);
+                context.fillRect(0, ray.r - 1, mag, 1);
+                const r = this.radius - this.tellDuration / 50;
+                if (r > 0) {
+                    context.fillRect(0, -r | 0, mag, 2 * r | 0);
+                }
+            context.restore();
+            //drawLaser(context, this.getHitRay(state), 0.1);
+            return;*/
+       }
+    }
+}
+
+const colorArray = ["rgba(207,35,64,0)", "rgba(207,35,64,0.2)", "rgba(207,35,64,0.8)", "rgba(239,59,59,1)", "rgba(249,200,200,1)", "rgba(249,200,200,1)", "rgba(239,59,59,1)", "rgba(207,35,64,0.8)", "rgba(207,35,64,0.2)", "rgba(207,35,64,0)"];
+const laserJitter = [0.03490, -0.03490 , 0.01745, -0.01745, 0, 0, 0];
+
+export function drawSingleColorLaser(context: CanvasRenderingContext2D, ray: Ray, alpha: number) {
+    context.save();
+        context.globalAlpha = alpha;
+        context.translate(ray.x1, ray.y1);
+        const dx = ray.x2 - ray.x1;
+        const dy = ray.y2 - ray.y1;
+        let theta = Math.atan2(dy, dx);
+		//Add some laser jitter
+		theta += Random.element(laserJitter);
+        context.rotate(theta);
+		//console.log(ray.r);
+		
+        // Pick a color. If this is too slow, we'll find a way to pick a color every N ticks or frames.
+		const randomColor = Random.element(colorArray);
+
+        // Set the fill style and draw a rectangle
+        context.fillStyle = randomColor;
+		context.beginPath();
+        const mag = Math.sqrt(dx * dx + dy *dy);
+        //context.fillRect(0, -ray.r, mag, ray.r * 2);
+        // Draw a circle at the start of the beam.
+		
+        context.arc(ray.r, 0, ray.r*2, 0, 2 * Math.PI);
+        // Draw a rectangle starting at the mid point of the circle.
+        context.rect(ray.r, -ray.r, mag - ray.r, ray.r * 2);
+        context.fill();
+		
     context.restore();
 }
