2a152d41aba8c2f9f73acfb2e5290c969495e6da
Chris Brewer <chris@chrisflash.com>
2025-11-30 16:47:51 -0600

    WIP: tree draft. Added utility for saving the current floor as an image.

--- app/content/objects/decoration.ts
+++ app/content/objects/decoration.ts
@@ -948,8 +948,13 @@ const windowOctogonal: DecorationType = {
     },
 };
 
-const cocoonFrame= requireFrame('gfx/objects/cocoon.png', {x: 0, y: 0, w: 24, h: 42, content: {x: 2, y: 22, w: 20, h: 20}});
-const cocoonBackFrame= requireFrame('gfx/objects/cocoon.png', {x: 24, y: 0, w: 24, h: 42, content: {x: 2, y: 22, w: 20, h: 20}});
+/*const podSouthGeometry = {w: 64, h: 48, content: {x: 20, y: 20, w: 24, h: 26}};
+const podSouthFrame = requireFrame('gfx/objects/podSouth.png', {x: 0, y: 0, ...podSouthGeometry});
+const podSouthDoorClosedFrame = requireFrame('gfx/objects/podSouth.png', {x: 0, y: 48, ...podSouthGeometry});
+const podSouthDoorOpenAnimation = createAnimation('gfx/objects/podSouth.png', podSouthGeometry, {y: 1, cols: 6});*/
+
+const cocoonFrame = requireFrame('gfx/objects/cocoon.png', {x: 0, y: 0, w: 24, h: 42, content: {x: 2, y: 22, w: 20, h: 20}});
+const cocoonBackFrame = requireFrame('gfx/objects/cocoon.png', {x: 24, y: 0, w: 24, h: 42, content: {x: 2, y: 22, w: 20, h: 20}});
 const cocoon: DecorationType = {
     render(context: CanvasRenderingContext2D, state: GameState, decoration: Decoration) {
         if (decoration.d === 'right') {
